5f904f6cf1ad17f1119b9f4b41a7d843f8dfeb94
giovanifrota <giovanifrota45@gmail.com>
2023-09-28 11:53:44 -0300

    Alterando o converter para, adicionando mais 2 tipos de moedas

--- Conversor de moedas/scripts.js
+++ Conversor de moedas/scripts.js
@@ -1,14 +1,17 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
+
 function convertValues(){
     const inputCurrency = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")//Valor em Real
     const currencyValueToConverted = document.querySelector(".currency-value")//Valor em Dolar
-
+    const currencyValueToConvertedLibra = document.querySelector(".currency-value-libra")//Valor em Libra
 
     const dolarToday = 4.0
     const euroToday = 5.2
+    const libraToday = 6.2
+    const bitcoinToday = 132.5
 
 
     if(currencySelect.value == "dolar"){
@@ -22,6 +25,18 @@ function convertValues(){
             style:"currency",
             currency:"EUR"
         }).format(inputCurrency/euroToday)
+    }
+    if(currencySelect.value == "libra"){
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-GB",{
+            style:"currency",
+            currency:"GBP"
+        }).format(inputCurrency/libraToday)
+    }
+    if(currencySelect.value == "bitcoin"){
+        currencyValueToConverted.innerHTML = new Intl.NumberFormat("en-US",{
+            styçe:"currency",
+            currency:"BTC"
+        }).format(inputCurrency/bitcoinToday)
     }
 
      currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
@@ -43,6 +58,18 @@ function changeCurrency(){
         currencyName.innerHTML = "Euro"
         currencyImage.src = "img/euro (1).png"
     }
+    if(currencySelect.value == "libra"){
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "img/libra.jpg"
+        currencyImage.width = "100"
+    }
+    if(currencySelect.value == "bitcoin"){
+        currencyName.innerHTML = "Bitcoin"
+        currencyImage.src = "img/bitcoin.jpg"
+        currencyImage.width = "100"
+    }
+
+    
 
     convertValues()
 
